e283308ac57433d3e551de098106803098097040
Robert Austin Haubrich <79111870+RAHaubrich@users.noreply.github.com>
2026-07-29 10:45:00 -0400

    Refactor DAG to use SparkSubmitOperator
    Will now run the function via a script called by the SparkSubmitOperator, in order to satisfy the requirements.

--- airflow/dags/streamflow_daily_summary.py
+++ airflow/dags/streamflow_daily_summary.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
-from airflow.decorators import dag, task
-from airflow.providers.standard.operators.bash import BashOperator
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 import sys
 from pathlib import Path
 
@@ -9,15 +9,13 @@ parent_dir = current_dir.parent.parent
 if str(parent_dir) not in sys.path:
     sys.path.append(str(parent_dir))
 
-from spark.jobs.daily_summary import create_transaction_details, create_transaction_summary, write_summary, write_transaction_details
-
 DEFAULT_ARGS = {
     "owner": "revature_wilmington_team",
     "retries": 4,
     "retry_delay": timedelta(minutes=10),
 }
 
-@dag(
+with DAG(
     dag_id = "daily_summary",
     default_args = DEFAULT_ARGS,
     description = "Returns a daily summary of the jobs completed.",
@@ -25,42 +23,13 @@ DEFAULT_ARGS = {
     schedule = '@daily',
     catchup = False,
     tags = ['summary', 'submit']
-)
-
-def streamflow_daily_summary():
-
-    # Create the task containing the summary creation functions
-    @task
-    def process_daily_data():
-
-        # Import additional modules
-        # Notably, do not leave pyspark open the entire time
-        from pyspark.sql import SparkSession
-        from spark.jobs.daily_summary import (
-            create_transaction_details, create_transaction_summary, 
-            write_summary, write_transaction_details
-        )
-
-        # Create spark session inside of the task since we're running it locally, would not work on a cluster
-        # To change it to a cluster, wrap the following in a script that runs its own Spark session
-        spark = SparkSession.builder.appName("LocalSummary").getOrCreate()
-
-        # Import data from the raw folder
-        new_data = spark.read.parquet("data/raw")
-
-        # Create a DataFrame containing the exact transaction details
-        details_df = create_transaction_details(new_data)
-        write_transaction_details(details_df)
-
-        # Create a summary DataFrame and write it to a parquet file
-        summary_df = create_transaction_summary(details_df)
-        write_summary(summary_df)
-
-        # Stop the spark instance
-        spark.stop()
-
-    # Call the task
-    process_daily_data()
+) as dag:
+    submit_summary = SparkSubmitOperator(
+        task_id="run_spark_submit_summary",
+        conn_id="spark_default",
+        application=f"{current_dir}/scripts/run_summary.py",
+        verbose = True
+    )
 
 # IMPORTANT: with overwrite, this will replace all the files from when it has run previously. As is, there is no duplicate prevention.
 # If the write functions were changed to append, this would have to change. Both ingested files and summary files to be split by date.
