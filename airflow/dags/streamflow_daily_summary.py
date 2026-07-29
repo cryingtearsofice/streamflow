3bd705fe65ddd678c64b14bffc98d25a8da60b99
Robert Austin Haubrich <79111870+RAHaubrich@users.noreply.github.com>
2026-07-29 16:28:43 -0400

    Epic 5 daily summary (#31)
    * Add run_summary.py for daily transaction summary
    
    Added script to be run within the DAG.
    
    * Create daily_summary DAG for job summaries
    
    This DAG returns a daily summary of completed jobs using a SparkSubmitOperator.
    
    * Refactor run_summary.py to load data directly
    
    Removed mock data creation and schema definition for new data loading.
    
    ---------
    
    Co-authored-by: Danielson A. <99297691+Dan-V31@users.noreply.github.com>

--- airflow/dags/streamflow_daily_summary.py
+++ airflow/dags/streamflow_daily_summary.py
@@ -1,8 +1,45 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import sys
+from pathlib import Path
+
+import os
+os.environ["HADOOP_HOME"] = "C:\\hadoop"
+
+os.environ["PYSPARK_PYTHON"] = sys.executable
+os.environ["PYSPARK_DRIVER_PYTHON"] = sys.executable
 
 from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.operators.bash import BashOperator
 
+current_dir = Path(__file__).resolve().parent
+parent_dir = current_dir.parent.parent
+if str(parent_dir) not in sys.path:
+    sys.path.append(str(parent_dir))
+
+DEFAULT_ARGS = {
+    "owner": "revature_wilmington_team",
+    "retries": 4,
+    "retry_delay": timedelta(minutes=10),
+}
+
+with DAG(
+    dag_id = "daily_summary",
+    default_args = DEFAULT_ARGS,
+    description = "Returns a daily summary of the jobs completed.",
+    start_date = datetime(2026, 7, 14),
+    schedule = '@daily',
+    catchup = False,
+    tags = ['summary', 'submit']
+) as dag:
+    submit_summary = SparkSubmitOperator(
+        task_id="run_spark_submit_summary",
+        conn_id="spark_default",
+        application=f"{current_dir}/scripts/run_summary.py",
+        verbose = True
+    )
+
+#May or may not have to delete this one. Keeping it for now.
 with DAG(
     dag_id="streamflow_daily_summary",
     description="Triggers the daily transaction summary Spark job",
@@ -14,3 +51,6 @@ with DAG(
         task_id="run_daily_summary",
         bash_command="spark-submit /opt/airflow/spark/jobs/daily_summary.py",
     )
+
+# IMPORTANT: with overwrite, this will replace all the files from when it has run previously. As is, there is no duplicate prevention.
+# If the write functions were changed to append, this would have to change. Both ingested files and summary files to be split by date.
---
+++ scripts/run_summary.py
@@ -0,0 +1,46 @@
+import sys
+from pathlib import Path
+
+import os
+os.environ["HADOOP_HOME"] = "C:\\hadoop"
+
+os.environ["PYSPARK_PYTHON"] = sys.executable
+os.environ["PYSPARK_DRIVER_PYTHON"] = sys.executable
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, LongType
+
+current_dir = Path(__file__).resolve().parent
+parent_dir = current_dir.parent
+if str(parent_dir) not in sys.path:
+    sys.path.append(str(parent_dir))
+
+from spark.jobs.daily_summary import create_transaction_details, create_transaction_summary, write_summary, write_transaction_details
+
+def main():
+    # Create the spark session
+    spark = SparkSession.builder \
+        .appName("LocalSummary") \
+        .config("spark.pyspark.python", sys.executable) \
+        .config("spark.pyspark.driver.python", sys.executable) \
+        .getOrCreate()
+    
+    try:
+        # Import data from the raw folder
+        project_root = parent_dir
+        new_data = spark.read.parquet(str(project_root / "data/raw"))
+
+        # Create a DataFrame containing the exact transaction details
+        details_df = create_transaction_details(new_data)
+        write_transaction_details(details_df, output_path=str(project_root / "data/curated/transaction_details"))
+
+        # Create a summary DataFrame and write it to a parquet file
+        summary_df = create_transaction_summary(details_df)
+        write_summary(summary_df, output_path=str(project_root / "data/curated/daily_summary"))
+    
+    finally:
+        # Stop the spark instance
+        spark.stop()
+
+if __name__ == "__main__":
+    main()
